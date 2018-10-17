533397900d3da8cf6f58bdef042902ff4bde586d
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-10-17 17:41:24 -0500

    refactored line 31

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     end 
 
     post '/login' do
-        @user = User.find_by(:username => params[:username])
+        @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
           session[:user_id] = @user.id
           redirect '/index'
