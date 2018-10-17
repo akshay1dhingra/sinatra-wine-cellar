974097e4a8eba140562a00848d77f7e79184fb81
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-10-17 14:37:14 -0500

    added post '/login' route in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,23 @@ class UsersController < ApplicationController
         if !logged_in? 
             erb :'users/signup'
         else
-            redirect "users/index"
+            redirect "/index"
         end
     end 
 
     get '/login' do 
         erb :'users/login'
     end 
+
+    post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect '/bottles'
+        else
+          redirect '/signup'
+        end
+      end
 
 
 end
