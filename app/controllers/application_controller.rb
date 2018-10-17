e001df8793cb80b7096f5acf9b0141c8584da172
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-10-16 22:56:48 -0500

    removed 'unless test?' from line 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions unless test?
+    enable :sessions 
     set :session_secret, "secret"
   end
 
