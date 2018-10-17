3653e2537fc7ad6f9d0d7c2e11020ec4904c822f
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-10-17 17:32:16 -0500

    fixed logout bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
         end
     end
 
-    post '/logout' do 
+    get '/logout' do 
         if logged_in?
             session.clear    
             redirect '/login'
