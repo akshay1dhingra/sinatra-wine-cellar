9bcfcbd3b598ed9bf8932fba5f41881ff3c3aeb6
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-10-18 19:04:46 -0500

    new migration

--- db/migrate/20181016212238_create_bottles.rb
+++ db/migrate/20181016212238_create_bottles.rb
@@ -2,7 +2,7 @@ class CreateBottles < ActiveRecord::Migration
   def change
     create_table :bottles do |t|
       t.string :name
-      t.string :type
+      t.string :grape
       t.integer :year
       t.string :location
       t.integer :user_id
